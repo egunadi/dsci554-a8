c65e9d97ef94e8b1b799b34926cb875ff9e7da9a
Eben Gunadi <egunadi@usc.edu>
2023-10-20 15:27:29 -0700

    Update js/line-chart.js to use d3 color scales

--- js/line-chart.js
+++ js/line-chart.js
@@ -29,19 +29,23 @@ function createLineChart() {
             .x(function(d) { return xScaleLine(d.Year); })
             .y(function(d) { return yScaleLine(d.GDP); });
 
+        // Extract unique country names from gdpData
+        const countries = Array.from(new Set(gdpData.map(d => d.Country)));
+
+        // Create D3 color scale
+        const colorScale = d3.scaleOrdinal()
+            .domain(countries)
+            .range(d3.schemeTableau10);
+
         svgLine.selectAll(".line")
             .data(countriesData)
             .enter().append("path")
             .attr("class", "line")
-            .style("fill", "none")  // Set fill to "none" to remove the area underneath the lines
+            .style("fill", "none")
             .attr("d", function(d) { return line(d.values); })
             .style("stroke", function(d) {
-                // Set stroke color based on country
-                if (d.key === "Japan") return "#E69F00";
-                if (d.key === "Korea") return "#56B4E9";
-                if (d.key === "Indonesia") return "#009E73";
-                if (d.key === "China") return "#F0E442";
-                if (d.key === "Singapore") return "#0072B2";
+                // Set stroke color based on country using the color scale
+                return colorScale(d.key);
             });
 
         svgLine.append("g")
@@ -71,65 +75,23 @@ function createLineChart() {
         const legend = svgLine.append("g")
             .attr("transform", `translate(10, 10)`);
 
-        legend.append("rect")
+        // Create legend items based on color scale
+        const legendItems = legend.selectAll(".legend-item")
+            .data(countries)
+            .enter()
+            .append("g")
+            .attr("class", "legend-item")
+            .attr("transform", (d, i) => `translate(0, ${i * 25})`);
+
+        legendItems.append("rect")
             .attr("width", 20)
             .attr("height", 20)
-            .attr("fill", "#E69F00");
+            .attr("fill", d => colorScale(d));
 
-        legend.append("rect")
-            .attr("width", 20)
-            .attr("height", 20)
-            .attr("fill", "#E69F00");
-
-        legend.append("text")
+        legendItems.append("text")
             .attr("x", 30)
             .attr("y", 10)
-            .text("Jpn");
-
-        legend.append("rect")
-            .attr("width", 20)
-            .attr("height", 20)
-            .attr("x", 90)
-            .attr("fill", "#56B4E9");
-
-        legend.append("text")
-            .attr("x", 120)
-            .attr("y", 10)
-            .text("Kor");
-
-        legend.append("rect")
-            .attr("width", 20)
-            .attr("height", 20)
-            .attr("y", 30)
-            .attr("fill", "#009E73");
-
-        legend.append("text")
-            .attr("x", 30)
-            .attr("y", 40)
-            .text("Ind");
-
-        legend.append("rect")
-            .attr("width", 20)
-            .attr("height", 20)
-            .attr("x", 90)
-            .attr("y", 30)
-            .attr("fill", "#F0E442");
-
-        legend.append("text")
-            .attr("x", 120)
-            .attr("y", 40)
-            .text("Chn");
-
-        legend.append("rect")
-            .attr("width", 20)
-            .attr("height", 20)
-            .attr("y", 60)
-            .attr("fill", "#0072B2");
-
-        legend.append("text")
-            .attr("x", 30)
-            .attr("y", 70)
-            .text("Sing");
+            .text(d => d);
     }).catch(function(error) {
         // Handle errors if the JSON file fails to load
         console.error("Error loading the data: " + error);
