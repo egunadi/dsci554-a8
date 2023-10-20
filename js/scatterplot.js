fa459ee15dc3bdef45e9d7728de1e12f6a2f64c0
Eben Gunadi <egunadi@usc.edu>
2023-10-20 09:06:03 -0700

    Update js/scatterplot.js to use color scale

--- js/scatterplot.js
+++ js/scatterplot.js
@@ -1,4 +1,3 @@
-// Create the bubble chart
 function createScatterplot() {
     var marginBubble = { top: 20, right: 20, bottom: 40, left: 60 };
     var widthBubble = document.getElementById("scatterplot").offsetWidth - marginBubble.left - marginBubble.right;
@@ -41,16 +40,30 @@ function createScatterplot() {
             .enter()
             .append("circle");
 
+        // Extract unique country names from populationData
+        const countries = Array.from(new Set(populationData.map(d => d.Country)));
+
+        // Create D3 color scale
+        const colorScale = d3.scaleOrdinal()
+            .domain(countries)
+            .range(d3.schemeTableau10);
+
+        // circles.attr("cx", function (d) { return xScaleBubble(d.Year); })
+        //     .attr("cy", function (d) { return yScaleBubble(d.Population); })
+        //     .attr("r", 1.5)
+        //     .style("fill", d => {
+        //             if (d.Country === "Japan") return "#E69F00" 
+        //             if (d.Country === "Korea") return "#56B4E9" 
+        //             if (d.Country === "Indonesia") return "#009E73" 
+        //             if (d.Country === "China") return "#F0E442" 
+        //             else return "#0072B2"
+        //         }); // Circle color based on data
+
         circles.attr("cx", function (d) { return xScaleBubble(d.Year); })
             .attr("cy", function (d) { return yScaleBubble(d.Population); })
-            .attr("r", 1.5)
-            .style("fill", d => {
-                    if (d.Country === "Japan") return "#E69F00" 
-                    if (d.Country === "Korea") return "#56B4E9" 
-                    if (d.Country === "Indonesia") return "#009E73" 
-                    if (d.Country === "China") return "#F0E442" 
-                    else return "#0072B2"
-                }); // Circle color based on data
+            .attr("r", 1.5) // Adjust the radius as needed
+            .style("fill", d => colorScale(d.Country)); // Circle color based on data
+
 
         // Add X-axis title
         svgBubble.append("text")
@@ -69,64 +82,27 @@ function createScatterplot() {
             .style("text-anchor", "middle")
             .text("Population");
 
-        // Add legend (you can customize this)
+        // Add legend using D3 color scale
         const legend = svgBubble.append("g")
+            .attr("class", "legend")
             .attr("transform", `translate(10, 10)`); // Adjust the translation values
 
-        legend.append("rect")
+        const legendItems = legend.selectAll(".legend-item")
+            .data(countries)
+            .enter()
+            .append("g")
+            .attr("class", "legend-item")
+            .attr("transform", (d, i) => `translate(0, ${i * 25})`); // Adjust the spacing between legend items
+
+        legendItems.append("rect")
             .attr("width", 20)
             .attr("height", 20)
-            .attr("fill", "#E69F00");
+            .attr("fill", d => colorScale(d)); // Use the color scale for legend items
 
-        legend.append("text")
+        legendItems.append("text")
             .attr("x", 30)
             .attr("y", 10)
-            .text("Jpn");
-
-        legend.append("rect")
-            .attr("width", 20)
-            .attr("height", 20)
-            .attr("x", 90)
-            .attr("fill", "#56B4E9");
-
-        legend.append("text")
-            .attr("x", 120)
-            .attr("y", 10)
-            .text("Kor");
-
-        legend.append("rect")
-            .attr("width", 20)
-            .attr("height", 20)
-            .attr("y", 30)
-            .attr("fill", "#009E73");
-
-        legend.append("text")
-            .attr("x", 30)
-            .attr("y", 40)
-            .text("Ind");
-
-        legend.append("rect")
-            .attr("width", 20)
-            .attr("height", 20)
-            .attr("x", 90)
-            .attr("y", 30)
-            .attr("fill", "#F0E442");
-
-        legend.append("text")
-            .attr("x", 120)
-            .attr("y", 40)
-            .text("Chn");
-
-        legend.append("rect")
-            .attr("width", 20)
-            .attr("height", 20)
-            .attr("y", 60)
-            .attr("fill", "#0072B2");
-
-        legend.append("text")
-            .attr("x", 30)
-            .attr("y", 70)
-            .text("Sing");
+            .text(d => d);
     }).catch(function(error) {
         // Handle errors if the JSON file fails to load
         console.error("Error loading the data: " + error);
